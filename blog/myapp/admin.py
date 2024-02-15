be67b6fd44289cf3e93a7c4bf385236e8b7c660d
Maksim Aleksanin <maksim.g00gle@gmail.com>
2024-02-15 19:14:52 +0400

    Lesson 5, admin panel, view and actions

--- blog/myapp/admin.py
+++ blog/myapp/admin.py
@@ -1,3 +1,55 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.http import HttpRequest
+from django.db.models import query
 
-# Register your models here.
+from .models import Coin, Author, Post
+
+
+@admin.action(description="Count authors")
+def authors_count(
+        model_admin: admin.ModelAdmin,
+        request: HttpRequest,
+        queryset: query.QuerySet
+) -> None:
+    messages.info(request, f"Count: {queryset.count()}")
+
+
+@admin.action(description="Posts themes")
+def get_themes(
+        model_admin: admin.ModelAdmin,
+        request: HttpRequest,
+        queryset: query.QuerySet
+) -> None:
+    themes = Post.objects.filter(author_id__in=queryset.values('pk').all()).values('category').all()
+    messages.info(
+        request,
+        f"Themes: "
+        f"{''.join(theme['category'] for theme in themes)}"
+    )
+
+
+class AuthorAdmin(admin.ModelAdmin):
+
+    fieldsets: list = [
+        (
+            "author",
+            {
+                "classes": ["wide"],
+                "fields": [("name", "surname")]
+            },
+        ),
+        (
+            "info",
+            {
+                "classes": ["collapse"],
+                "fields": ["biography", "email"]
+            },
+        ),
+    ]
+    search_fields = ["name"]
+    actions = [authors_count, get_themes]
+
+
+admin.site.register(Coin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post)
--- blog/myapp/models.py
+++ blog/myapp/models.py
@@ -46,6 +46,9 @@ class Author(models.Model):
     def fullname(self):
         return f"{self.name} {self.surname}"
 
+    def __str__(self):
+        return f"{self.fullname}(author)"
+
 
 class Post(models.Model):
 
